9afdfe6f5bf831485adeb97fd7333abce3d8b8d4
dastiles <dastilesforever@gmail.com>
2023-11-17 13:48:28 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        appleGreen: "#a8c316",
+        antiFlashWhite: "#f3f3f3",
+        darkViolet: "#664ea7",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+};
